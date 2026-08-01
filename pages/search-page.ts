38713771d3bff4d2cc5a7ea5f1edcfc44a4bb85a
Army-R <80074550+Army-R@users.noreply.github.com>
2026-08-01 15:07:02 -0600

    Search 'Actions'

--- pages/search-page.ts
+++ pages/search-page.ts
@@ -4,21 +4,32 @@ export class SearchPage {
     readonly page: Page;
     readonly getSearchButton: Locator;
     readonly docSearchModal: Locator;
+    readonly docSearchInput: Locator;
 
     constructor(page: Page) {
         this.page = page;
         this.getSearchButton = page.getByRole('button', {name: 'Search'})
         this.docSearchModal = page.locator('.DocSearch-Modal');
+        this.docSearchInput = page.locator('#docsearch-input')
     }
 
     async clickSearchButton() {
         await this.getSearchButton.click();
-    }
-
-    async assertSearchModal() {
         await expect(this.docSearchModal).toBeVisible();
     }
 
+    async searchActions(){
+        await expect(this.docSearchInput).toBeVisible();
+        await this.docSearchInput.fill('Actions');
+    }
+
+    async pressEnter() {
+        await this.docSearchInput.press('Enter');
+    }
+
+    async assertActionsSearch() {
+        await expect(this.page).toHaveURL('/docs/input')
+    }
 }
 
 export default SearchPage
